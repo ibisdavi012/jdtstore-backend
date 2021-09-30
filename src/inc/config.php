666ebe660d2c8eccef0487c6079eee5bdd940172
Ibis David Márquez Bouttó <ibisdavi012@gmail.com>
2021-09-30 17:20:05 -0400

    Update database settings

--- src/inc/config.php
+++ src/inc/config.php
@@ -1,17 +1,10 @@
 <?php
 
 // Database Settings
-/*define('DATABASE_HOST', 'localhost');
+define('DATABASE_HOST', 'localhost');
 define('DATABASE_NAME', 'id17682141_jdtstore');
 define('DATABASE_USER', 'id17682141_root');
-define('DATABASE_PASSWORD', 'Qb/Ad7_2VNAX(Cc?');*/
-
-define('DATABASE_HOST', 'localhost');
-define('DATABASE_NAME', 'jdtstore');
-define('DATABASE_USER', 'root');
-define('DATABASE_PASSWORD', '');
-
-
+define('DATABASE_PASSWORD', 'Qb/Ad7_2VNAX(Cc?');
 
 // HTTP headers
 define('HTTP200', '"HTTP/1.1 200 OK"');
